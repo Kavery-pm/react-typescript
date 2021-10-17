b002bf4a181747945af410e7b9cf89fcfa6c40e5
Kavery pm <shiny.kavery@gmail.com>
2021-10-17 09:35:52 -0300

    added a todo data model

--- 07-react-ts-starting-project/src/App.tsx
+++ 07-react-ts-starting-project/src/App.tsx
@@ -6,7 +6,7 @@ import Todo from "./components/Todo";
 function App() {
   return (
     <div>
-      <Todo item={[]}></Todo>
+      <Todo item={['Learn js', 'learn ts', 'learn react', 'learn redux']}></Todo>
     </div>
   );
 }
