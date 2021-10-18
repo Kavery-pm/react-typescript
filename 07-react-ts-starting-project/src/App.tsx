55c638f9af6b7bf39077cf249cfcd314cce0156c
Kavery pm <shiny.kavery@gmail.com>
2021-10-18 02:35:37 -0300

    added useState using typescript

--- 07-react-ts-starting-project/src/App.tsx
+++ 07-react-ts-starting-project/src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Todos from "./models/Todo";
 
@@ -6,13 +6,16 @@ import Todo from "./components/Todo";
 import NewTodo from "./components/NewTodo";
 
 function App() {
-  const todos = [
-    new Todos("Learn js"),
-    new Todos("learn ts"),
-    new Todos("learn redux"),
-  ];
+  // const todos = [
+  //   new Todos("Learn js"),
+  //   new Todos("learn ts"),
+  //   new Todos("learn redux"),
+  // ];
+const [todos, settodos] = useState<Todos[]>([]);
   const addTodoHandler = (todo:string)=>{
+    const newTodo = new Todos(todo);
 console.log(todo);
+settodos(prevState => prevState.concat(newTodo));
   }
   return (
     <div>
